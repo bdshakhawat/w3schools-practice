01824168f017e966cc81a8872545e010f0dd8fa2
bdshakhawat <mdshakwat.hossain@gmail.com>
2022-09-30 20:55:48 +0600

    second commit

--- w3school.js
+++ w3school.js
@@ -1,4 +1,7 @@
 // Javascript Function
+
+
+// Function Syntax
 function muFunc(names,time){
     console.log(names + " am sleeping from " + time);
 }
@@ -6,6 +9,17 @@ muFunc('Rahim', '10pm');
 muFunc('Karim', '11pm');
 muFunc('Jasim', '12pm');
 console.log("My name is Shakhawat");
+
+// Function Return (Return means get out form the function)
+function myFunc(profession,designation){
+    console.log('I am a physics ' +   profession + " and working as a " + designation);
+    return profession + designation;
+    // Return should be the last statement in a function
+    console.log(3);
+}
+myFunc('Teacher', 'Lecture');
+
+
 
 
 
