fd03258e887c0ec6067de51c1337615115b661e6
bdshakhawat <mdshakwat.hossain@gmail.com>
2022-09-30 20:38:50 +0600

    first commit

---
+++ w3school.js
@@ -0,0 +1,15 @@
+// Javascript Function
+function muFunc(names,time){
+    console.log(names + " am sleeping from " + time);
+}
+muFunc('Rahim', '10pm');
+muFunc('Karim', '11pm');
+muFunc('Jasim', '12pm');
+console.log("My name is Shakhawat");
+
+
+
+
+
+
+
